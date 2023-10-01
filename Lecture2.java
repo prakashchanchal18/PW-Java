899048692b0522f6d16f8dcf34f5fe398fe8d297
“prakashchanchal18” <prakashchanchal13@icloud.com>
2023-10-01 14:48:55 +0530

    JAVA and IntelliJ IDEA Setup

--- Lecture2.java
+++ Lecture2.java
@@ -2,6 +2,7 @@ package physicsWallah;
 
 public class Lecture2 {
     public static void main(String[] args) {
+        System.out.println("Lecture 2");
         System.out.println("JAVA and IntelliJ IDEA Setup");
     }
 }
